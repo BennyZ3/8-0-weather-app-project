ea025f3f4bc50756852a25a8a62a50bcb38bcc46
BennyZ3 <bennyz3@hotmail.com>
2021-10-08 11:29:05 -0400

    reorganized with functions and implemented history callback

--- main.js
+++ main.js
@@ -5,30 +5,21 @@ document.querySelector("form").addEventListener("submit", (event) => {
     document.querySelector(".results").textContent = "Error: No text entered";
   }
   document.querySelector("form").reset();
-  let url = `https://wttr.in/~${search}?format=j1`;
+  let url = `https://wttr.in/${search}?format=j1`;
   fetch(url)
     .then((response) => {
       response.json().then((object) => {
-        let results = document.querySelector(".results");
-        let area = object.nearest_area[0].areaName[0].value;
-        let region = object.nearest_area[0].region[0].value;
-        let country = object.nearest_area[0].country[0].value;
-        let current = object.current_condition[0].FeelsLikeF;
-        // console.log(area, region, country);
-        results.innerHTML = `<h2>${area}</h2><p><b>Area:</b> ${area}</p><p><b>Region:</b> ${region}</p><p><b>Country:</b> ${country}</p><p><b>Currently:</b> Feels like ${current}°F</p>`;
+        results(object);
+        //assigning history
         document.querySelector(".history p").textContent = "";
         let newUrl = document.createElement("li");
-        newUrl.innerHTML = `<a href='${url}'>${search}- ${current}°F</a>`;
+        newUrl.innerHTML = `<a href="javascript:history('${url}')" value='${url} name='test'>${object.nearest_area[0].areaName[0].value}- ${object.current_condition[0].FeelsLikeF}°F</a>`;
+        newUrl.addEventListener("onclick", (event) => {
+          history(event.target.test.value);
+        });
         document.querySelector(".history ol").appendChild(newUrl);
-        document.querySelector(".days").innerHTML = "";
         // console.log(object.weather);
-        let days = ["Today", "Tomorrow", "Day After Tomorrow"];
-        for (let i = 0; i < days.length; i++) {
-          let newDiv = document.createElement("div");
-          newDiv.append(dailyTemp(days[i], object.weather[i]));
-          document.querySelector(".days").append(newDiv);
-        }
-        console.log(dailyTemp("today", object.weather[0]));
+        // console.log(dailyTemp("today", object.weather[0]));
       });
     })
     .catch(console.log);
@@ -40,4 +31,31 @@ function dailyTemp(day, object) {
     <p><b>Max Temperature:</b> ${object.maxtempF}°F</p>
     <p><b>Min Temperature:</b> ${object.mintempF}°F</p>`;
   return newDiv;
+}
+
+function results(object) {
+  let results = document.querySelector(".results");
+  //assigning the results box values
+  let area = object.nearest_area[0].areaName[0].value;
+  let region = object.nearest_area[0].region[0].value;
+  let country = object.nearest_area[0].country[0].value;
+  let current = object.current_condition[0].FeelsLikeF;
+  results.innerHTML = `<h2>${area}</h2><p><b>Area:</b> ${area}</p><p><b>Region:</b> ${region}</p><p><b>Country:</b> ${country}</p><p><b>Currently:</b> Feels like ${current}°F</p>`;
+  //days display
+  document.querySelector(".days").innerHTML = "";
+
+  let days = ["Today", "Tomorrow", "Day After Tomorrow"];
+  for (let i = 0; i < days.length; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.append(dailyTemp(days[i], object.weather[i]));
+    document.querySelector(".days").append(newDiv);
+  }
+}
+
+function history(url) {
+  fetch(url).then((response) => {
+    response.json().then((object) => {
+      results(object);
+    });
+  });
 }
